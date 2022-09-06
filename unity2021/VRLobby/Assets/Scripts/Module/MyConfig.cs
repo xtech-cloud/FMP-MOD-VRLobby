08a688988404cfb066f922e789100be6d3073b97
Easlee Liu <easlee@outlook.com>
2022-09-07 02:16:59 +0800

    regenerate

--- unity2021/VRLobby/Assets/Scripts/Module/MyConfig.cs
+++ unity2021/VRLobby/Assets/Scripts/Module/MyConfig.cs
@@ -8,10 +8,39 @@ namespace XTC.FMP.MOD.VRLobby.LIB.Unity
     /// </summary>
     public class MyConfig : MyConfigBase
     {
+        public class BannerEntry
+        {
+            [XmlElement("Banner")]
+            public UiElement banner { get; set; } = new UiElement();
+            [XmlArray("Subjects"), XmlArrayItem("Subject")]
+            public Subject[] subjects { get; set; } = new Subject[0];
+        }
+
+        public class BannerMenu
+        {
+            [XmlElement("Title")]
+            public UiElement title { get; set; }
+
+            [XmlAttribute("distance")]
+            public float distance { get; set; } = 2;
+
+            [XmlAttribute("space")]
+            public float space { get; set; } = 20;
+
+            [XmlArray("BannerEntries"), XmlArrayItem("BannerEntry")]
+            public BannerEntry[] entries { get; set; } = new BannerEntry[0];
+        }
+
         public class Style
         {
             [XmlAttribute("name")]
             public string name { get; set; } = "";
+
+            [XmlAttribute("active")]
+            public string active { get; set; } = "";
+
+            [XmlElement("BannerMenu")]
+            public BannerMenu bannerMenu { get; set; } = new BannerMenu();
         }
 
 
--- unity2021/VRLobby/Assets/Scripts/Module/MyInstance.cs
+++ unity2021/VRLobby/Assets/Scripts/Module/MyInstance.cs
@@ -15,16 +15,9 @@ namespace XTC.FMP.MOD.VRLobby.LIB.Unity
     /// </summary>
     public class MyInstance : MyInstanceBase
     {
-
+        private List<Transform> enterButtons = new List<Transform>();
         public MyInstance(string _uid, string _style, MyConfig _config, LibMVCS.Logger _logger, Dictionary<string, LibMVCS.Any> _settings, MyEntryBase _entry, MonoBehaviour _mono, GameObject _rootAttachments)
             : base(_uid, _style, _config, _logger, _settings, _entry, _mono, _rootAttachments)
-        {
-        }
-
-        /// <summary>
-        /// 应用样式
-        /// </summary>
-        public void ApplyStyle()
         {
         }
 
@@ -33,6 +26,62 @@ namespace XTC.FMP.MOD.VRLobby.LIB.Unity
         /// </summary>
         public void HandleCreated()
         {
+            enterButtons.Clear();
+            rootUI.transform.Find("BannerMenu").gameObject.SetActive(style_.active.Equals("BannerMenu"));
+            if (style_.active.Equals("BannerMenu"))
+            {
+                // 设置距离
+                var position = rootUI.transform.localPosition;
+                position.z = style_.bannerMenu.distance;
+                rootUI.transform.localPosition = position;
+
+                var tTitle = rootUI.transform.Find("BannerMenu/imgTitle");
+                alignByAncor(tTitle, style_.bannerMenu.title.anchor);
+                loadSpriteFromTheme(style_.bannerMenu.title.image, (_sprite) =>
+                {
+                    tTitle.GetComponent<Image>().sprite = _sprite;
+                });
+
+                var tEntry = rootUI.transform.Find("BannerMenu/entry");
+                tEntry.gameObject.SetActive(false);
+                for (int i = 0; i < style_.bannerMenu.entries.Length; i++)
+                {
+                    var entry = style_.bannerMenu.entries[i];
+                    var clone = GameObject.Instantiate(tEntry.gameObject, tEntry.parent);
+                    clone.transform.localPosition = new Vector3(i * style_.bannerMenu.space, 0, 0);
+                    clone.SetActive(true);
+                    alignByAncor(clone.transform, entry.banner.anchor);
+                    loadSpriteFromTheme(entry.banner.image, (_sprite) =>
+                    {
+                        clone.GetComponent<Image>().sprite = _sprite;
+                        clone.transform.Find("__gaze__").GetComponent<Image>().sprite = _sprite;
+                    });
+
+                    var tEnter = clone.transform.Find("enter");
+                    tEnter.gameObject.SetActive(false);
+                    enterButtons.Add(tEnter);
+
+                    clone.GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        foreach(var button in enterButtons)
+                        {
+                            button.gameObject.SetActive(button == tEnter);
+                        }
+                    });
+
+                    tEnter.GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        foreach(var subject in entry.subjects)
+                        {
+                            publishSubject(subject);
+                        }
+                    });
+
+                    //TODO 以摄像机为中心计算旋转角度
+                }
+
+
+            }
         }
 
         /// <summary>
@@ -57,5 +106,24 @@ namespace XTC.FMP.MOD.VRLobby.LIB.Unity
         {
             rootUI.gameObject.SetActive(false);
         }
+
+        private void publishSubject(MyConfig.Subject _subject)
+        {
+            var dummyModel = (entry_ as MyEntry).getDummyModel();
+            var data = new Dictionary<string, object>();
+            foreach (var parameter in _subject.parameters)
+            {
+                if (parameter.type.Equals("string"))
+                    data[parameter.key] = parameter.value;
+                else if (parameter.type.Equals("int"))
+                    data[parameter.key] = int.Parse(parameter.value);
+                else if (parameter.type.Equals("float"))
+                    data[parameter.key] = float.Parse(parameter.value);
+                else if (parameter.type.Equals("bool"))
+                    data[parameter.key] = bool.Parse(parameter.value);
+            }
+            dummyModel.Publish(_subject.message, data);
+        }
+
     }
 }
